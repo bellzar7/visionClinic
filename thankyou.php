1bebff2ec9ddacaa9f958237ca7eb7deae674524
Nazariy Belelya <belelya.nazar1999@gmail.com>
2024-10-13 17:24:39 +0300

    first commit

---
+++ thankyou.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gracias</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="vh-100 d-flex justify-content-center align-items-center">
+            <div>
+                <div class="mb-4 text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                    </svg>
+                    
+                </div>
+                <div class="text-center">
+                    <h1>¡Gracias!</h1>
+                    <p>Gracias por contactarnos. Ya estamos procesando su solicitud y nos pondremos en contacto con usted en breve para darle una respuesta o información adicional. <br>Su paciencia es muy apreciada. </p>
+                    <a class="btn btn-primary" style="background-color: #212529; border-color: #212529;" href="index.html">De vuelta a casa</a>
+                </div>
+            </div>
+    </body>
+
+</html>
